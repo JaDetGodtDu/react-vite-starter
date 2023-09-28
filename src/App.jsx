9e7888ce6e4afc00efb85f705f514030b2144e9e
ReubenSkimmel <123992392+JaDetGodtDu@users.noreply.github.com>
2023-09-28 11:47:55 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,33 @@
 import reactLogo from "./assets/react.svg";
 
 function App() {
-    return (
-        <>
-            <header>
-                <a href="https://react.dev" target="_blank" rel="noreferrer">
-                    <img src={reactLogo} className="logo react" alt="React logo" />
-                </a>
-                <h1>RACE Your React 🎉</h1>
-            </header>
-        </>
-    );
+  const [members, setMembers] = useState([
+    { firstName: "Peter", lastName: "Lind" },
+  ]);
+
+  return (
+    <>
+      <header>
+        <a href="https://react.dev" target="_blank" rel="noreferrer">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+        <h1>RACE Your React 🎉</h1>
+      </header>
+      {members.map((member) => (
+        <Member member={member} key={member.id} />
+      ))}
+    </>
+  );
+}
+
+function Member({ member }) {
+  return (
+    <article>
+      <h2>
+        {member.firstName} {member.lastName}
+      </h2>
+    </article>
+  );
 }
 
 export default App;
